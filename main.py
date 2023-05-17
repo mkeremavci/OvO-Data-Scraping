d6a7304267b7ff1d3ebd2ad265a3b88617f44db2
kerem <kerem@cpu-2xlarge-1.europe-west6-a.c.vitamu-ml.internal>
2023-05-17 08:05:08 +0000

    test against 429

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 from tqdm import tqdm
 import os
-
+import time
 
 with open('meta.json', 'r') as f:
     meta_data = json.load(f)
@@ -16,7 +16,15 @@ else:
 
 
 url = "https://api-sonuc.oyveotesi.org/api/v1/submission/school"
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Referer': 'https://tutanak.oyveotesi.org/',
+    'Sec-Ch-Ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    }
 
+counter = 0
 
 for ckey, city in tqdm(list(meta_data.items())):
     if ckey in scraped:
@@ -40,15 +48,23 @@ for ckey, city in tqdm(list(meta_data.items())):
             for skey, sid in neigh.items():
                 scraped[ckey][dkey][nkey][skey] = {}
                 
+                time.sleep(0.5)
                 sUrl = f"{url}/{sid}"
-                response = requests.get(sUrl)
+                response = requests.get(sUrl, headers=headers)
+                print(response)
                 data = response.json()
+
+                counter += 1
+                if counter % 10 == 0:
+                    counter = 0
+                    time.sleep(2)
                 
                 for d in data:
                     scraped[ckey][dkey][nkey][skey][d['ballot_box_number']] = {
                         'cm_result': d['cm_result'],
                         'mv_result': d['mv_result'],
                     }
+                
 
         with open('data.json', 'w') as f:
             f.write(json.dumps(scraped, indent=4))
