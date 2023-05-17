a715ef68aaa2730f0e7ac60ba135819d786605ef
keremdotmu <kerem@vitamu.com>
2023-05-17 09:26:27 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # OvO-Data-Scraping
+
+### Collected Data Links
+
+["14 Mayıs 2023 Cumhurbaşkanlığı Seçimi Oy ve Ötesi Açıklanan Sonuçlar"](https://drive.google.com/file/d/1ucE5IQZeiLNdK-ROgOpF91bXGTfQkljV/view?usp=sharing)
+
+["14 Mayıs 2023 Cumhurbaşkanlığı Seçimi Oy ve Ötesi Tutanak Linkleri"](https://drive.google.com/file/d/1G2lPwNvOp3zi9CdQKA-W8pi0XioGVFBn/view?usp=sharing)
+
+["14 Mayıs 2023 Milletvekili Genel Seçimi Oy ve Ötesi Açıklanan Sonuçlar"](https://drive.google.com/file/d/1cQTRffSD-1hYPj--KoU5ggYRxPh67l-2/view?usp=sharing)
+
+["14 Mayıs 2023 Milletvekili Genel Seçimi Oy ve Ötesi Tutanak Linkleri"](https://drive.google.com/file/d/1q6N6MHOXfHTEGNDpGjTU3CF_TV3v7KPF/view?usp=sharing)
---
+++ collect_meta.py
@@ -0,0 +1,45 @@
+import requests
+import json
+from tqdm import trange, tqdm
+import os
+
+
+if os.path.exists('meta.json'):
+    with open('meta.json', 'r') as f:
+        meta_data = json.load(f)
+else:
+    meta_data = {}
+    
+
+url = "https://api-sonuc.oyveotesi.org/api/v1"
+
+
+for i in trange(len(meta_data) + 1, 82):
+    # İlçeler
+    cUrl = f"{url}/cities/{i}/districts"
+    response = requests.get(cUrl)
+    data = response.json()
+    
+    meta_data[i] = {x['name']: {} for x in data}
+    nkeys = [x['id'] for x in data]
+    nnames = [x['name'] for x in data]
+    
+    # Mahalleler
+    for nkey, nname in zip(nkeys, nnames):
+        nUrl = f"{cUrl}/{nkey}/neighborhoods"
+        response = requests.get(nUrl)
+        data = response.json()
+        
+        meta_data[i][nname] = {x['name']: {} for x in data}
+        skeys = [x['id'] for x in data]
+        snames = [x['name'] for x in data]
+        
+        for skey, sname in zip(skeys, snames):
+            sUrl = f"{nUrl}/{skey}/schools"
+            response = requests.get(sUrl)
+            data = response.json()
+            
+            meta_data[i][nname][sname] = {x['name']: x['id'] for x in data}
+    
+    with open('meta.json', 'w') as f:
+        f.write(json.dumps(meta_data, indent=4))
